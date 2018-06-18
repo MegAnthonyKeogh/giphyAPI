e81b8e7bb9aa659ac7dc172aa22393a6d45e6f68
Megan Anthony <megananthony@Megans-MacBook-Air.local>
2018-06-18 16:50:28 -0400

    API is working

--- assets/javascript/gify.js
+++ assets/javascript/gify.js
@@ -2,24 +2,36 @@ alert("connected!");
 
 
 var animalBtns = ["cow", "dog", "cat", "donkey", "rabbit", "skunk", "parrot", "beaver", "eagle"];
-var animal = "cat";
+animal = ;
+
+
 function displayGifs() {
 
-
-    //var animal = $(this).attr("data-name");
+ var animal = $(this).attr("data-name");
   
-    var queryURL = $.get("https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=qYVT6phufVUaWUoCVaZ4s9DJkEe1VJvB");
+ var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=qYVT6phufVUaWUoCVaZ4s9DJkEe1VJvB";
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-        console.log(response);
-        console.log("working");
-
-    });
-}
+        
+            var animalDiv = $("<div class='animal'>");
+          
+            //loop through all the gifs provided
+            for (var i = 0; i< response.data.length; i++){
+          //retrieves URL for image
+            var imgURL = response.data[i].images.original.url;
+            // creates image 
+            var image = $("<img>").attr("src", imgURL);
+          
+            // Aattachs the images
+             animalDiv.append(image);
+          
+            // adds to div 
+           $("#popGifs").prepend(animalDiv);
+          }
+          })
  
-
 
 
 // render buttons 
@@ -43,10 +55,41 @@ function displayGifs() {
       a.text(animalBtns[i]);
       // Adding the button to the buttons-view div
       $("#animalButtons").append(a);
-    }
+    } 
+  }
 
-    
-  };
+  //create buttons from input
+ 
+  $("#add-gif").on("click", function(event){
+    event.preventDefault();
+    // This line grabs the input from the textbox
+    var newAnimal = $(this).val().trim();
+    var a = $("<button>");
+    // Adding a class of movie-btn to our button
+    a.addClass("animal-btn");
+    a.addClass("animal")
+    // Adding a data-attribute
+    a.attr("data-name", animalBtns[i]);
+    a.att("data-name", animal[i]);
+    // Providing the initial button text
+    a.text(animalBtns[i]);
+    // Adding the button to the buttons-view div
+    $("#animalButtons").append(a);
 
+    // Adding animal to the animal buttons array 
+    animalsBtns.push(newAnimal);
+
+    // Calling renderButtons which handles the processing of our movie array
+    renderButtons();
+  }
+
+  // Adding a click to add gifs after btns are rendered
+  $(document).on("click", ".animal", displayGifs);
+
+
+//called functions 
   renderButtons();
   displayGifs();
+  createGifs();
+
+    
